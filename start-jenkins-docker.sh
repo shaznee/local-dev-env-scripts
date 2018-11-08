db2dd4842ffd3aeba2e5d693cd8df3c627385793
SHAZNEE <mrmshaznee@hotmail.com>
2018-11-07 18:42:08 -0800

    Run container in detached mode and comment added to retrieve local admin password

--- start-jenkins-docker.sh
+++ start-jenkins-docker.sh
@@ -6,11 +6,12 @@ set +x
 docker kill local-jenkins
 docker rm local-jenkins
 
-docker run --name local-jenkins -p 8080:8080 -p 50000:50000 --rm -u root \
+docker run --name local-jenkins -d -i \
+-p 8080:8080 -p 50000:50000 --rm -u root \
 -v $(which docker):/usr/bin/docker \
 -v /var/run/docker.sock:/var/run/docker.sock \
 -v ~/jenkins_home:/var/jenkins_home \
 jenkins
 
 # Run the following command after the container starts to retieve the admin password
-# docker exec -it local-jenkins bash
+# docker exec -it local-jenkins cat /var/jenkins_home/secrets/initialAdminPassword
